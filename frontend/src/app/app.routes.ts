af6f7c047ec9b6c358325a66801700a2205e6ce7
Alexyr28 <alexyr24@gmail.com>
2026-07-29 21:47:48 -0500

    feat: Nuevo menu

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -42,6 +42,20 @@ export const routes: Routes = [
       import('./features/reportes/reportes.component').then((m) => m.ReportesComponent),
   },
   {
+    path: 'credito-hipotecario',
+    loadComponent: () =>
+      import('./features/credito-hipotecario/credito-hipotecario.component').then(
+        (m) => m.CreditoHipotecarioComponent,
+      ),
+  },
+  {
+    path: 'reduccion-credito',
+    loadComponent: () =>
+      import('./features/reduccion-credito/reduccion-credito.component').then(
+        (m) => m.ReduccionCreditoComponent,
+      ),
+  },
+  {
     path: 'proyectos',
     loadComponent: () =>
       import('./features/proyectos/proyectos-listado.component').then(
